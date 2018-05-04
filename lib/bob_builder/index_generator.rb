329482137591da24869c31c5cc303f97e883ebc6
kw7oe <choongkwern@hotmail.com>
2018-05-04 14:43:05 +0800

    Add in simple rendering

--- lib/bob_builder/index_generator.rb
+++ lib/bob_builder/index_generator.rb
@@ -2,10 +2,30 @@ module BobBuilder
   class IndexGenerator
     def initialize(source_dir)
       @source_dir = source_dir
+      @dir_lists = []
       @file_lists = []
     end
 
-    def render
+    # Render the index from an array of files
+    def render(file_lists)
+      previous_parent_dir = nil
+
+      file_lists.map do |f|
+        heading = nil
+        name = File.basename(f, '.md').gsub("-", " ")
+        parent_dir = File.dirname(f)
+          .split("/")
+          .last
+          .gsub("-", " ")
+          .capitalize
+
+        if (previous_parent_dir != parent_dir)
+          previous_parent_dir = parent_dir
+          heading = "### #{parent_dir}\n"
+        end
+
+        "#{heading}- [#{name}](#{f.sub('.md', '.html')})"
+      end.join("\n")
     end
 
     def file_lists
--- test/bob_builder/index_generator_test.rb
+++ test/bob_builder/index_generator_test.rb
@@ -8,14 +8,39 @@ describe BobBuilder::IndexGenerator do
   end
 
   it 'should render index correctly' do
+    result = generator.render([
+      "notes/another-title.md",
+      "notes/this.md"
+    ])
+    expected_result = <<~EOF.strip
+    ### Notes
+    - [another title](notes/another-title.html)
+    - [this](notes/this.html)
+    EOF
+
+    assert_equal expected_result, result
+  end
+
+  it 'should render index correctly with nested folder' do
+    result = generator.render(generator.file_lists)
+    expected_result = <<~EOF.strip
+    ### Notes
+    - [another title](notes/another-title.html)
+    - [this is title](notes/this-is-title.html)
+    ### Topic one
+    - [ch 2](notes/topic-one/ch-2.html)
+    - [ch 1](notes/topic-one/ch-1.html)
+    EOF
+
+    assert_equal expected_result, result
   end
 
   it 'should return file lists correctly' do
     result = generator.file_lists
-    expected_file_lists = ['notes/another-title.md', 'notes/this-is-title.md', 'notes/topic_one/ch-1.md', 'notes/topic_one/ch-2.md']
+    expected_result = ['notes/another-title.md', 'notes/this-is-title.md', 'notes/topic-one/ch-1.md', 'notes/topic-one/ch-2.md']
 
 
-    assert_equal result.sort, expected_file_lists.sort
+    assert_equal result.sort, expected_result.sort
   end
 
 end
